0b5729c32a076d77fc4f1e23d7996da3153d2c2a
Matthew Curran <mjcurran@ucdavis.edu>
2023-11-28 14:38:40 -0500

    feat:  First commit, basic framework for local environment.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mongo_django_framework
+
+This is a base Django app framework to support:  
+Django  
+PostgresDB  
+Celery  
+MongoDB  
---
+++ config/settings/local.py
@@ -0,0 +1,36 @@
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="VIzsoezAe9LLS4uRW1MkfmTqz7rvD1uHf6M1KGJn1YDyx3N3R9gAStlN9ZjcxZts",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+# CACHES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
+    }
+}
+
+# WhiteNoise
+# ------------------------------------------------------------------------------
+# http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
+INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS
+
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+if env("USE_DOCKER") == "yes":
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
